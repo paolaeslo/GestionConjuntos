1e60e8e746bc50e0283bc942e4f404e35f57d135
Paola E. Lopez <paolaeslo23@gmail.com>
2022-11-02 10:12:56 -0500

    Se crea proyecto microservicio registrar usuarios utilizando el framework Spring boot basado en java

---
+++ src/main/java/com/conjuntosresidenciales/ConjuntosresidencialesApplication.java
@@ -0,0 +1,13 @@
+package com.conjuntosresidenciales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConjuntosresidencialesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConjuntosresidencialesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/conjuntosresidenciales/ConjuntosresidencialesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conjuntosresidenciales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConjuntosresidencialesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
